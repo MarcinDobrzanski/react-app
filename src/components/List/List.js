83781ea2c3a87caeaa3f8a41f527d2f8c19a7830
Marcin <marcin@dobrzanski.me>
2023-02-16 15:31:38 +0100

    Add style modle

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,19 +1,21 @@
+import styles from './List.module.scss';
+
 const List = () => {
   return (
-    <div>
-      <header>
+    <div className={styles.list}>
+      <header className={styles.header}>
         <h2>Things to do<span>soon</span></h2>
       </header>
-      <p>Interesting things I want to check out</p>
-      <section>
+      <p className={styles.description}>Interesting things I want to check out</p>
+      <section className={styles.columns}>
         <article>
-          <h2>Books</h2>
+          <h2 className={styles.title}>Books</h2>
         </article>
         <article>
-          <h2>Movies</h2>
+          <h2 className={styles.title}>Movies</h2>
         </article>
         <article>
-          <h2>Games</h2>
+          <h2 className={styles.title}>Games</h2>
         </article>
       </section>
     </div>
