eff301602f950b57618ffc913b043e60d5d56cd9
Marcin <marcin@dobrzanski.me>
2023-02-17 12:46:41 +0100

    Add container component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,7 +6,7 @@ const SearchForm = () => {
     return (
         <form className={styles.searchForm}>
             <TextInput placeholder="Search…" />
-            <Button text="Search" />
+            <Button>Search</Button>
         </form>
     );
 };
