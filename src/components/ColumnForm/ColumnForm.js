2b10cf9a36ee12302025e8086bc8c9e01330fc04
Marcin <marcin@dobrzanski.me>
2023-02-20 17:58:14 +0100

    Add component CardForm

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -4,6 +4,7 @@ import Button from './../Button/Button';
 import TextInput from './../TextInput/TextInput';
 
 
+
 const ColumnForm = props => {
   const [title, setTitle] = useState('');
   const [icon, setIcon] = useState('');
