6e9f7f7f80a23d01d4223f33964ff5965d005b49
Marcin <marcin@dobrzanski.me>
2023-03-09 19:55:26 +0100

    Add action creators

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,14 @@ import shortid from 'shortid';
 import strContains from '../utils/strContains'
 
 //selectors
-  export const getFilteredCards = ({ cards, searchString }, columnId) => cards
+export const getFilteredCards = ({ cards, searchString }, columnId) => cards
   .filter(card => card.columnId === columnId && strContains(card.title, searchString));
+export const getAllColumns = state => state.columns;
 
-  export const getAllColumns = state => state.columns;
+// action creators
+export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
+export const addCard = payload => ({ type: 'ADD_CARD', payload });
+export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload });
 
 
 const reducer = (state, action) => {
