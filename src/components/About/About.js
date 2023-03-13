99a1de7143806c401477357fef84cd0b14811084
Marcin <marcin@dobrzanski.me>
2023-03-13 19:35:22 +0100

    Add component Home

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,14 @@
-import styles from './NavBar.module.scss';
-import Container from './../Container/Container';
+import styles from './About.module.scss';
 import PageTitle from '../PageTitle/PageTitle';
 
 const About = () => {
   return (
-    <Container className={styles.container}>
+    <div className={styles.hero}>
       <PageTitle>About</PageTitle>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
       </p>
-    </Container>
+    </div>
   );
 };
 
--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,15 +1,14 @@
-import styles from './NavBar.module.scss';
-import Container from './../Container/Container';
+import styles from './Favorite.module.scss';
 import PageTitle from '../PageTitle/PageTitle';
 
 const Favorite = () => {
   return (
-    <Container className={styles.container}>
+    <div className={styles.hero}>
       <PageTitle>Favorite</PageTitle>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
       </p>
-    </Container>
+    </div>
   );
 };
 
